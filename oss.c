83570a1cd4d8a2e18c62b9fa77bfc92006d0bd74
sefik mehmedovic <mehmedov@hoare7.cs.umsl.edu>
2018-10-10 01:21:16 -0500

    Added statesment to prevent more than 20 process, set the defaults, setup sharedmemory, and everything compiles

--- oss.c
+++ oss.c
@@ -8,20 +8,24 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <semaphore.h>
 
 // page ~534 for semaphores in book 
 // 
 // shared memory key and size of shared memory.. size of an int(4 bytes)
 #define SHMKEY 321800
+#define SHMKEYB 321801
 #define BUFF_SZ	sizeof ( int )
 
 //shared memory function
-//void sharedMemory(int n, int s);
+void sharedMemory(int n, int s);
+void signalHandler(int sig);
 
 int main(int argc, char* argv[])
 {
   	int input, s, l, t; // s total number of children 5 is default.. l is logfile... t is time default i 2
-	
+	char *fileName = "log.out";
+
 	while ((input = getopt (argc, argv, "hs:l:t:")) != -1)
 	
 	{
@@ -32,7 +36,7 @@ int main(int argc, char* argv[])
 		printf("\nTo run the program use ./oss\n");
 		printf("-h is for help and all options\n ");
 	    	printf("-s x where x is the max number of user process spawned (default 5)\n");
-       		printf("-l filename is the name of the log file");
+       		printf("-l filename is the name of the log file\n");
         	printf("-t z is where z is the time in seconds when master will terminate itself");
 		printf(" and all children(default 2 seconds)\n");
        		printf("Example: ./oss -s 10 (runs oss with 10 child process)\n");
@@ -41,6 +45,15 @@ int main(int argc, char* argv[])
 
 	case 's':
        		 s = atoi(optarg);
+            if(s > 20)
+            {
+              printf("\nS can not be greater than 20. S was set to 20\n");
+              s = 20;
+            }
+	   else if(s == 0)
+		{
+		 	s = 5;
+		}
         	 printf("case s: %d\n", s);
      	break;
     
@@ -51,6 +64,10 @@ int main(int argc, char* argv[])
 	case 't':
 		t = atoi(optarg);
 		printf("case t: %d",t);
+		if(t == 0)
+		{
+			t = 2;
+		}
 	break;
 
    	case '?':
@@ -65,6 +82,47 @@ int main(int argc, char* argv[])
 	 }
 	
 	}
-  
+
+// variables for shared memory
+// Clock[0]  is seconds clock[1] is nanoseconds
+// shmMsg is the msg containing child PID seconds and nanoseconds
+  int *clock;
+  int *shmMsg;
+  	
+
+
+
 	return 0;
 }
+
+void sharedMemory(int n, int s)
+{
+    //  printf("sharedmemory: \n");
+    int count = 0;
+  // get shared memory segment ID.. not 100% of this 
+   int shmid = shmget (SHMKEY,BUFF_SZ , 0711 | IPC_CREAT );
+   if (shmid == -1)
+   {
+     printf("OSS:shmid Error in shmget \n");
+     exit(1);
+   } 
+  
+  int shmidB = shmget(SHMKEYB,BUFF_SZ, 0711 | IPC_CREAT );
+	if(shmidB == -1)
+	{
+	printf("OSS:shmidB Error in shmget\n ");
+	exit(1);
+	}
+   
+   char * paddr = ( char * ) (shmat (shmid, NULL, 0) );
+   int * pint = (int *) (paddr);
+}
+
+void signalHandler(int sig)
+{
+  if( sig == SIGINT)
+  {
+    printf("OSS: CTRL+C Caught: Memory Clean Up");
+  }
+}
+
--- user.c
+++ user.c
@@ -1,7 +1,33 @@
+//child
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+
+// shared memory key and size of shared memory.. size of an int(4 bytes)
+#define SHMKEY 321800
+#define BUFF_SZ	sizeof ( int )
+
+void childMemory();
+
 
 int main(int argc, char* argv[]) 
 {
-	printf("user");
+	
 	return 0;
+}
+
+void childMemory()
+{
+  //int getValue;	
+
+  int shmid = shmget (SHMKEY,BUFF_SZ , 0711 | IPC_CREAT );
+  if (shmid == -1)
+   {
+    printf("Worker: Error in shmget..\n");
+    return 1;
+   }
+  int *cint = ( shmat (shmid, NULL, 0) );
+ 
 }
